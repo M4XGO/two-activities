ec2ba47a12421eb7f5c2c293e6b4018f4cb24478
Maxime <maxgo.maxoue@gmail.com>
2024-06-06 12:55:37 +0200

    view 2 and connection

--- app/src/main/java/fr/kody/twoactivities/SecondActivity.java
+++ app/src/main/java/fr/kody/twoactivities/SecondActivity.java
@@ -1,11 +1,6 @@
 package fr.kody.twoactivities;
 
-import static androidx.core.content.PackageManagerCompat.LOG_TAG;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void launchSecondActivity(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
-        Log.d(LOG_TAG, "Button clicked!");
     }
 }
